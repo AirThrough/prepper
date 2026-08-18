9a84c13f4fba4b6d1a7964723d718a1d1735261a
Andrey Skorodumov <andrey.sskoro@gmail.com>
2026-08-18 04:29:09 +0300

    basic folder setup

---
+++ .cursor/skills/ui-components/UI_COMPONENTS.md
@@ -0,0 +1,10 @@
+---
+name: ui-components
+description: describes general intructions on how to manage ui components in the repo
+---
+
+# Adding a component
+
+1. Make sure we have shadcn/ui component for the needed elements of ui. If absent and there are suitable ones available - install.
+2. New components should be added to shared/ui/{ComponentName}
+3. should use index file exports in the way that I can import any component from shared/ui
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { AppSidebar } from "@/widgets/AppSidebar"
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      test
+    <div className="flex h-full">
+      <AppSidebar />
+      <main className="flex-1">
+        <div className="h-full w-full">
+          <h1 className="text-2xl font-bold">Welcome to the app</h1>
+        </div>
+      </main>
     </div>
   )
 }
